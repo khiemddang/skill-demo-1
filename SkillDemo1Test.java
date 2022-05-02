f9f975073545b65a35a0edfa26c536461572ff89
Khiem Dang <103155859+khiemddang@users.noreply.github.com>
2022-05-01 22:11:17 -0700

    Edited test file

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -5,6 +5,6 @@ public class SkillDemo1Test {
     
     @Test
     public void multiplication() {
-        assertEquals(5, SkillDemo1.multiply(2, 3));     // Test fails initially
+        assertEquals(6, SkillDemo1.multiply(2, 3));     // Test should succeed now
     }
 }
